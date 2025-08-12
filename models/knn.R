518296ee8be86abaad07ba1746551d68c508f1e8
24-bee-supply <24-bee-supply@icloud.com>
2025-08-11 22:36:36 -0700

    Added conclusions.

--- models/knn.R
+++ models/knn.R
@@ -1,36 +1,3 @@
-library(caret)
-library(doParallel)
-
-set.seed(202)
-
-cl <- makeCluster(11)  
-registerDoParallel(cl)
-
-# Prepare data
-X_train <- model.matrix(relevance ~ . - 1, data = train_transformed)
-y_train <- as.factor(train_transformed$relevance)
-
-# Specify larger k values to test
-# Start higher since large k is doing better
-k_values <- data.frame(
- k = seq(143, 159, by = 2)
-)
-
-# Run KNN with 10-fold CV
-# Add preProcess to scale within each fold (prevents data leakage)
-knn_model <- train(
- x = X_train,
- y = y_train,
- method = "knn",
- preProcess = c("center", "scale"),  # Scale within each CV fold
- trControl = trainControl(method = "cv", number = 10),
- tuneGrid = k_values
-)
-
-# Save model
-saveRDS(knn_model, "../models/knn_model.rds")
-stopCluster(cl)
-
-# Print basic info
-cat("Best k:", knn_model$bestTune$k, "\n")
-cat("CV Accuracy:", max(knn_model$results$Accuracy), "\n")
+version https://git-lfs.github.com/spec/v1
+oid sha256:44038cd1c949f76c64c5924ae202884c6cc3c6fdf0540a21a56017a9159e9346
+size 866
--- models/lasso.R
+++ models/lasso.R
@@ -1,35 +1,3 @@
-library(glmnet)
-
-set.seed(202)
-
-# Prepare data
-X_train <- model.matrix(relevance ~ . ^2  - 1, data = train_transformed)
-y_train <- train_transformed$relevance
-
-# Run LASSO with 10-fold CV
-# standardize = TRUE (default) ensures scaling happens within each CV fold
-cv_lasso <- cv.glmnet(
-  x = X_train,
-  y = y_train,
-  family = "binomial",
-  alpha = 1,
-  nfolds = 10,
-  type.measure = "class",
-  standardize = TRUE  # Explicitly set to TRUE (scales within each fold)
-)
-
-# Fit final model with optimal lambda
-# Again with standardize = TRUE for consistency
-lasso_final <- glmnet(X_train, y_train, 
-                      family = "binomial", 
-                      alpha = 1, 
-                      lambda = cv_lasso$lambda.min,
-                      standardize = TRUE)
-
-# Save results
-lasso_results <- list(cv_model = cv_lasso, final_model = lasso_final)
-saveRDS(lasso_results, "../models/lasso_model.rds")
-
-# Print basic info
-cat("Best lambda:", cv_lasso$lambda.min, "\n")
-cat("CV Accuracy:", 1 - min(cv_lasso$cvm), "\n")
+version https://git-lfs.github.com/spec/v1
+oid sha256:cd2b0254af37ea9ec7df26571867b7522a2f8c3fb908da03f94421feae809338
+size 1021
